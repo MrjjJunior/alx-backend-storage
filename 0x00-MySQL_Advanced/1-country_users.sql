048ee05500274acd3dfab9ddfdb4954f4da15b51
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-17 10:14:55 +0200

    Counties

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
-
+-- creates table users adds a column of countries enumeations
 CREATE TABLE IF NOT EXISTS users (
     id INT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-    country 
-)
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
