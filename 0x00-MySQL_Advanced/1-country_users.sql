f20354f8e06c32e4be5d72868ba9a66d89b220c7
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-17 13:04:23 +0000

    fix file

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- enumerartion of countries
 
 CREATE TABLE IF NOT EXISTS users (
-    id INT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country enum('US', 'CO', 'TN') DEFAULT 'US' NOT NULL
