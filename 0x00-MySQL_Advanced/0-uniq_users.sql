c0721003d0ab8e08b552334fb44610bc82a5b94c
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-17 07:30:00 +0000

    comments

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,7 @@
-
+-- creates table if it doesnt exist
 CREATE TABLE IF NOT EXISTS users (
     id INT  NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name  VARCHAR(255)
 );
+
