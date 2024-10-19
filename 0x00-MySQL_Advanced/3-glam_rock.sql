22ddf76076852f89cf649a259461fd6ea5e1bd5d
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-19 21:37:50 +0200

    procedure

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,15 +1,11 @@
--- List Glam rock bands ranked by their longevity (lifespan in years until 2022)
-SELECT 
+-- SQL script to list all Glam rock bands ranked by their longevity
+
+SELECT
     band_name,
-    CASE
-        -- If the band has split, calculate lifespan from formed year to split year
-        WHEN split IS NOT NULL THEN (split - formed)
-        -- If the band hasn't split, calculate lifespan from formed year to 2022
-        ELSE (2022 - formed)
-    END AS lifespan
-FROM 
+    IFNULL(2022 - formed, 0) AS lifespan
+FROM
     metal_bands
-WHERE 
+WHERE
     style = 'Glam rock'
-ORDER BY 
+ORDER BY
     lifespan DESC;
--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,16 +1,18 @@
 -- Script create a procedure that computes and updates average score
 DELIMITER $$
 
+DROP  PROCEDURE IF EXISTS update_average_score;
+
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
     DECLARE avg_score FLOAT;
-
-    -- caluculate average score for user
+    
+    -- Calculate the average score for the given user
     SELECT AVG(score) INTO avg_score
     FROM corrections
     WHERE user_id = user_id;
 
-    -- update the users table with the new average score
+    -- Update the users table with the new average score
     UPDATE users
     SET average_score = avg_score
     WHERE id = user_id;
