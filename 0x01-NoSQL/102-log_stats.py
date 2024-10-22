e86d17435d8c2d5bf5adefe44cfe53936b3d2e1d
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-22 14:13:32 -0700

    logs

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,4 +1,37 @@
 #!/usr/bin/env python3
+""" Top 10 most present Ips  on the collection """
 from pymongo import MongoClient
 
 
+def log_stats():
+    """ Provides stats about Nginx logs """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs
+    collection = db.nginx
+
+    log_count = collection.count_documents({})
+    print(f"{log_count} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for method in methods:
+        method_count = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_count}")
+
+    m = {"method": "GET", "path": "/status"}
+    status_check_count = collection.count_documents(m)
+    print(f"{status_check_count} status check")
+
+    print("IPs:")
+    top_ips = collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+
+    for ip in top_ips:
+        print(f"\t{ip['_id']}: {ip['count']}")
+
+
+if __name__ == "__main__":
+    log_stats()
