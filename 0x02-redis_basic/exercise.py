5ca2ee34362a7d67042b199a7e30d6f23bfaa84e
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-23 23:12:50 +0200

    count calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 """ Redis script that returns a random key """
-
-
-import redis 
+import redis
 from uuid import uuid4
 from functools import wraps
 from typing import Any, Callable,  Optional, Union
+
+
+def count_calls(method: Callable) -> Callable:
+    """ call count
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """ Wrapping called method
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
 
 class Cache:
     '''  '''
@@ -15,8 +26,6 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    #@call_history
-    #@count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''  '''
         key = str(uuid4())
@@ -24,7 +33,7 @@ class Cache:
         client.set(key, data)
         return key
 
-    
+
 def get(self, key: str, fn: Optional[Callable] = None) -> Any:
     '''  '''
     client = self._redis
@@ -40,34 +49,10 @@ def get(self, key: str, fn: Optional[Callable] = None) -> Any:
         return fn(value)
     return value
 
-
     def get_str(self, data: bytes) -> str:
         '''  '''
         return data.decode("UTF-8")
-    
 
     def get_int(self, data: bytes) -> str:
         '''  '''
         return int(data)
-    
-
-#    def count_calls(self, methods: Callabe) -> Callable:
-#        '''  '''
-#        @wraps(method)
-#        def wrapper(self: Any, *args, **kwargs) -> str:
-#            '''  '''
-#            self.redis.incr(method.__qualname__)
-#            return method(self, *args, **kwargs)
-#        return wrapper
-
-
-def count_calls(method: Callable) -> Callable:
-    """ call count
-    """
-    @wraps(method)
-    def wrapper(self: Any, *args, **kwargs) -> str:
-        """ Wrapping called method
-        """
-        self._redis.incr(method.__qualname__)
-        return method(self, *args, **kwargs)
-    return wrapper
