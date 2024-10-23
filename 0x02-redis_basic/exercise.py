861413e366863a004ae9d056a14db12a179e92ee
Tshepiso <tlhongtshepiso2@gmail.com>
2024-10-24 00:47:28 +0200

    History

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,36 @@ from functools import wraps
 from typing import Any, Callable,  Optional, Union
 
 
+def call_history(method: Callable) -> Callable:
+    def wrapper(self, *args, **kwargs) -> Any:
+        # Create input and output list keys
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+        
+        # Normalize and store the input arguments
+        self._redis.rpush(input_key, str(args))
+        
+        # Call the original method and get the output
+        output = method(self, *args, **kwargs)
+        
+        # Store the output in the outputs list
+        self._redis.rpush(output_key, output)
+        
+        return output
+    return wrapper
+
+
+def count_calls(method: Callable) -> Callable:
+    """ call count
+    """
+    @wraps(method)
+    def wrapper(self: Any, *args, **kwargs) -> str:
+        """ Wrapping called method
+        """
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
 
 class Cache:
     '''  '''
@@ -17,8 +47,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    #@call_history
-    #@count_calls
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         '''  '''
         key = str(uuid4())
